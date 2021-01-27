226bbedd1085e4fdb0fe0bde990139add062d633
SukruMertOz <77850643+SukruMertOz@users.noreply.github.com>
2021-01-27 16:25:40 +0300

    Add project files.

---
+++ Classes/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public int MyProperty { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+
+    }
+
+
+
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demirog";
+            customer.City = "Ankara";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City = "İstanbul", FirstName = "Derin",LastName="Demirog"
+
+
+
+            };
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
+        }
+    }
+}
